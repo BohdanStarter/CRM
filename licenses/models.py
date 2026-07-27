04ac2d1614e1a1238a9d8ba88e89fe46354163c7
BohdanStarter <bogdan2cs2@gmail.com>
2026-07-27 18:44:11 +0000

    License model tests

--- licenses/models.py
+++ licenses/models.py
@@ -1,7 +1,7 @@
 import secrets
 import string
+from dateutil.relativedelta import relativedelta
 from django.utils import timezone
-from datetime import timedelta
 from django.db import models
 from products.models import Product
 from customers.models import Customer
@@ -20,7 +20,7 @@ def generate_license_key():
 class License(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="licenses")
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="licenses")
-    license_key = models.CharField(max_length=LICENSE_KEY_LENGTH, unique=True, editable=False)
+    license_key = models.CharField(max_length=19, unique=True, editable=False)
     ACTIVE = 'Active'
     INACTIVE = 'Inactive'
     SUSPENDED = 'Suspended'
@@ -35,13 +35,20 @@ class License(models.Model):
     note = models.TextField(blank=True)
     purchase_date = models.DateTimeField(auto_now_add=True)
     expiration_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
     def save(self, *args, **kwargs):
         if not self.license_key:
             self.license_key = self.generate_unique_license_key()
 
-        if not self.expiration_date:
+        if self.expiration_date is None and self.product.billing_type != Product.LIFETIME:
             self.expiration_date = self.expiration()
+
+        # if self.is_expired:
+        #     self.status = self.EXPIRED
+
         super().save(*args, **kwargs)
 
     def generate_unique_license_key(self):
@@ -54,15 +61,21 @@ class License(models.Model):
         if self.product.billing_type == Product.LIFETIME:
             expiration_date = None
         elif self.product.billing_type == Product.MONTHLY:
-            expiration_date = timezone.now() + timedelta(days=30)
+            expiration_date = timezone.now() + relativedelta(months=1)
         elif self.product.billing_type == Product.ANNUALLY:
-            expiration_date = timezone.now() + timedelta(days=365)
+            expiration_date = timezone.now() + relativedelta(years=1)
         else:
             raise ValueError("Unknown value of billing type.")
         return expiration_date
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+    # def update_status(self):
+    #     if self.is_expired:
+    #         self.status = self.EXPIRED
+    #         self.save()
+    #     else:
+    #         pass
+    #     return self.status
 
     @property
     def is_expired(self):
--- licenses/tests.py
+++ licenses/tests.py
@@ -1,3 +1,147 @@
 from django.test import TestCase
-
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
+from datetime import timedelta
+import re
+from licenses.models import License
+from products.models import Product
+from customers.models import Customer
 # Create your tests here.
+
+
+class LicenseTestCase(TestCase):
+    def setUp(self):
+        test_customer = Customer.objects.create(
+            full_name="Test Customer",
+            email="test@gmail.com",
+            status=Customer.ACTIVE
+        )
+        product_lifetime = Product.objects.create(
+            name="VPN Unlimited",
+            description="VPN protection for your devices",
+            category=Product.SECURITY,
+            billing_type=Product.LIFETIME,
+            status=Product.ACTIVE,
+            price="100"
+        )
+        product_month = Product.objects.create(
+            name="VPN 1 month",
+            description="VPN protection for your devices",
+            category=Product.SECURITY,
+            billing_type=Product.MONTHLY,
+            status=Product.ACTIVE,
+            price="10"
+        )
+        product_year = Product.objects.create(
+            name="VPN 1 year",
+            description="VPN protection for your devices",
+            category=Product.SECURITY,
+            billing_type=Product.ANNUALLY,
+            status=Product.ACTIVE,
+            price="35"
+        )
+        License.objects.create(
+            customer=test_customer,
+            product=product_lifetime,
+            status=License.ACTIVE,
+        )
+        License.objects.create(
+            customer=test_customer,
+            product=product_month,
+            status=License.ACTIVE,
+        )
+        License.objects.create(
+            customer=test_customer,
+            product=product_year,
+            status=License.ACTIVE,
+        )
+
+    def test_key_generated(self):
+        product_lifetime = Product.objects.get(name="VPN Unlimited")
+        product_year = Product.objects.get(name="VPN 1 year")
+        product_month = Product.objects.get(name="VPN 1 month")
+
+        lifetime = License.objects.get(product=product_lifetime)
+        year = License.objects.get(product=product_year)
+        month = License.objects.get(product=product_month)
+        print("Method: test_key_generated passed the test.")
+        self.assertTrue(lifetime.license_key)
+        self.assertTrue(year.license_key)
+        self.assertTrue(month.license_key)
+
+
+
+    def test_expiration_date(self):
+        product_lifetime = Product.objects.get(name="VPN Unlimited")
+        product_year = Product.objects.get(name="VPN 1 year")
+        product_month = Product.objects.get(name="VPN 1 month")
+
+        lifetime = License.objects.get(product=product_lifetime)
+        year = License.objects.get(product=product_year)
+        month = License.objects.get(product=product_month)
+        print("Method: test_expiration_date passed the test.")
+
+        # Check if expiration date is added automatically
+        self.assertTrue(year.expiration_date)
+        self.assertTrue(month.expiration_date)
+
+        # Check if expiration date set properly
+        self.assertIsNone(lifetime.expiration_date)
+        self.assertAlmostEqual(year.expiration_date, timezone.now() + relativedelta(years=1), delta=timedelta(seconds=2))
+        self.assertAlmostEqual(month.expiration_date, timezone.now() + relativedelta(months=1), delta=timedelta(seconds=2))
+
+    def test_is_expired(self):
+        product_lifetime = Product.objects.get(name="VPN Unlimited")
+        product_year = Product.objects.get(name="VPN 1 year")
+        product_month = Product.objects.get(name="VPN 1 month")
+
+        lifetime = License.objects.get(product=product_lifetime)
+        year = License.objects.get(product=product_year)
+        month = License.objects.get(product=product_month)
+
+        self.assertFalse(lifetime.is_expired)
+        self.assertFalse(year.is_expired)
+        self.assertFalse(month.is_expired)
+
+    def test_is_expired_expired(self):
+        product_year = Product.objects.get(name="VPN 1 year")
+        product_month = Product.objects.get(name="VPN 1 month")
+
+        year = License.objects.get(product=product_year)
+        month = License.objects.get(product=product_month)
+
+        year.expiration_date=(timezone.now() - relativedelta(days=1))
+        year.save()
+        month.expiration_date=(timezone.now() - relativedelta(days=1))
+        month.save()
+
+        self.assertTrue(year.is_expired)
+        self.assertTrue(month.is_expired)
+
+    def test_license_key(self):
+        product_lifetime = Product.objects.get(name="VPN Unlimited")
+        product_year = Product.objects.get(name="VPN 1 year")
+        product_month = Product.objects.get(name="VPN 1 month")
+
+        lifetime = License.objects.get(product=product_lifetime)
+        year = License.objects.get(product=product_year)
+        month = License.objects.get(product=product_month)
+
+        key_structure = r"[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}"
+
+        self.assertTrue(bool(re.fullmatch(key_structure, lifetime.license_key)))
+        self.assertTrue(bool(re.fullmatch(key_structure, year.license_key)))
+        self.assertTrue(bool(re.fullmatch(key_structure, month.license_key)))
+
+
+
+
+
+
+
+
+
+
+
+
+
